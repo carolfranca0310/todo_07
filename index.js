cc57a2b4e98f314e54993b347fcdedb693c1cf8d
Caroline <carolfranca0310@gmail.com>
2022-08-02 18:46:01 -0300

    correção do exercicio

--- index.js
+++ index.js
@@ -1,47 +1,26 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const readline = require("readline")
 
-app.get('/', (req, res) => {
-  //configurando prompt de comando
-  const readline = require("readline").createInterface({
-    input:process.stdin,
-    output:process.stdout
-  });
 
-  //texto que aparece quando o usuário entra na rota
-  readline.setPrompt('Adicione uma propriedade CSS! \n');
-  //espaço pra digitar uma resposta
-  readline.prompt();
 
-  //vetor em que as propriedades CSS que o usuário digitar vão ser armazenadas
-  let propriedades = []
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
 
-    //função que será executada quando o usuário der enter depois de inserir a propriedade nova
-    readline.on('line', function (input){
-      if (input !== "sair"){
-        propriedades.push(input)
-        readline.prompt();
-      } else {
-        readline.close()
-      }
-    }).on ('close', function(){
-      propriedades.sort(function(a,b){
-        var propA = a.toLowerCase(), propB = b.toLwerCase();
-        if (propA < propB)
-        return -1;
-        if (propA>propB)
-        return 1;
-        return 0;
-      });
-      //com a função for Each mostro cada propriedade que o usuário digitou, agora já ordenada
-      propriedades.forEach(propriedade => console.log(propriedade))
-      propriedades = []
-      process.exit(0);
-    });
+let lista = []
+function listar(){
+  rl.question("Digite uma propriedade CSS: ", resposta => {
+    if (resposta.trim().toUpperCase() !== 'SAIR'){
+      lista.push(resposta)
+      listar()
+    } else{
+      rl.close()
+      console.log(lista.sort())
+    }
+  })
+}
 
-})
+listar()
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+
+//Karlla, tava tudo errado e Bruno me ajudou, beijos <3 mudei o código inteiro 
