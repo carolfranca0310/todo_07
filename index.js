795f794e5b4ffa3a4eea78f4ba476654674ccc35
Caroline <carolfranca0310@gmail.com>
2022-08-02 10:51:15 -0300

    adição de comentários

--- index.js
+++ index.js
@@ -3,16 +3,21 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
+  //configurando prompt de comando
   const readline = require("readline").createInterface({
     input:process.stdin,
     output:process.stdout
   });
 
+  //texto que aparece quando o usuário entra na rota
   readline.setPrompt('Adicione uma propriedade CSS! \n');
+  //espaço pra digitar uma resposta
   readline.prompt();
 
+  //vetor em que as propriedades CSS que o usuário digitar vão ser armazenadas
   let propriedades = []
 
+    //função que será executada quando o usuário der enter depois de inserir a propriedade nova
     readline.on('line', function (input){
       if (input !== "sair"){
         propriedades.push(input)
@@ -29,6 +34,7 @@ app.get('/', (req, res) => {
         return 1;
         return 0;
       });
+      //com a função for Each mostro cada propriedade que o usuário digitou, agora já ordenada
       propriedades.forEach(propriedade => console.log(propriedade))
       propriedades = []
       process.exit(0);
